0d9f77f2dd99174635ba5e2290e9c0f9da826aff
medfahmy <medfahmy99@gmail.com>
2021-08-24 15:15:37 +0100

    data loaders

---
+++ server/src/utils/createUpdootLoader.ts
@@ -0,0 +1,16 @@
+import DataLoader from "dataloader";
+import { Updoot } from "../entities/Updoot";
+
+export const createUpdootLoader = () =>
+  new DataLoader<{ postId: number; userId: number }, Updoot | null>(
+    async (keys) => {
+      const updoots = await Updoot.findByIds(keys as any);
+      const updootIdToUpdoot: Record<string, Updoot> = {};
+
+      updoots.forEach((u) => {
+        updootIdToUpdoot[`${u.userId}|${u.postId}`] = u;
+      });
+
+      return keys.map((key) => updootIdToUpdoot[`${key.userId}|${key.postId}`]);
+    }
+  );
